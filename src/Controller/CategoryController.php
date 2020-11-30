6f0ad20c14441bd84eedcfb8c94588404b125bc5
JeanneSic <siche.jeanne@gmail.com>
2020-11-30 12:16:30 +0100

    Quest 8 creation of Category Controller + views + adding @param on methods show

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,59 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/categories", name = "category_")
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
+    public function index(): Response
+    {
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+
+        return $this->render('category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * Getting a category by its name
+     * @Route("/{categoryName}", methods={"GET"}, name="show")
+     * @param string $categoryName
+     * @return Response A response instance
+     */
+    public function show(string $categoryName) {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            -> findOneBy( ['name' => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException("Error 404 - Aucune catégorie ne correspond à " . $categoryName);
+        }
+
+        $programsByCategory = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findBy(
+                ['category' => $category->getId()],
+                ['id' => 'DESC'],
+                3
+            );
+
+        return $this->render('category/show.html.twig', ['programsByCategory' => $programsByCategory]);
+    }
+
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     /**
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
+    public function index(): Response
+    {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
+        return $this->render('programs/index.html.twig', [
+            'programs' => $programs,
+        ]);
+    }
+
+    /**
+     * Getting a program by id
+     *
      * @Route("/show/{id}", requirements={"id"="\d+"}, methods={"GET"}, name = "show")
+     * @param int $id
+     * @return Response
      */
     public function show(int $id): Response
     {
-        return $this->render('programs/show.html.twig', ['id' => $id]);
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException('No program with id : '.$id.' found in program\'s table.');
+        }
+
+        return $this->render('programs/show.html.twig', ['program' => $program]);
     }
 
 }
