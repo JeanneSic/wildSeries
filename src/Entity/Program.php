83583f808d4075a99ddef7b5c63ee0c6884410d7
JeanneSic <siche.jeanne@gmail.com>
2020-12-07 14:15:37 +0100

    Quest15 - adding actors on form to add/edit a program

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -60,6 +60,7 @@ class Program
     public function __construct()
     {
         $this->seasons = new ArrayCollection();
+        $this->actors = new ArrayCollection();
     }
 
     public function getId(): ?int
--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -2,8 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Actor;
 use App\Entity\Program;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,8 +17,15 @@ class ProgramType extends AbstractType
         $builder
             ->add('title', TextType::class)
             ->add('summary', TextType::class)
-            ->add('poster', TextType::class)
+            ->add('poster', UrlType::class)
             ->add('category', null, ['choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class'=> Actor::class,
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+                'by_reference' => false,
+            ])
         ;
     }
 
